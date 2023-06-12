a0f24bf63362d57310079b58fd4394a9fbfb1b45
casio15 <tomer1452002@gmail.com>
2023-06-13 01:07:36 +0300

    Added controller and service files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { GalleryModule } from './gallery/gallery.module';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { CommenteryModule } from './commentery/commentery.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, GalleryModule, AuthModule, DatabaseModule, CommenteryModule],
   controllers: [AppController],
   providers: [AppService],
 })

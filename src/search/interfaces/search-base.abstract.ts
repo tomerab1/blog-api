bcde055021b04a21d088803f5306f6ed116a345f
casio15 <tomer1452002@gmail.com>
2023-07-11 23:06:33 +0300

    Started adding search capabilites with elasticsearch

---
+++ src/search/interfaces/search-base.abstract.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
+import Search from './search.interface';
+
+@Injectable()
+export default abstract class SearchBase<T extends Search> {
+  constructor(private readonly elasticService: ElasticsearchService) {}
+
+  async query(search: T) {
+    return await this.elasticService.asyncSearch();
+  }
+}
--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
 import { SearchService } from './search.service';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ElasticsearchModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        node: configService.get('ELASTICSEARCH_URL'),
+        auth: {
+          username: configService.get('ELASTICSEARCH_USR_NAME'),
+          password: configService.get('ELASTICSEARCH_PASSWORD'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [SearchService],
+  exports: [ElasticsearchModule],
 })
 export class SearchModule {}
